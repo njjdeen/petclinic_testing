38dc22a5e6146b914d4427500517b55c1b500342
njjdeen <97450369+njjdeen@users.noreply.github.com>
2022-02-25 10:47:30 +0100

    Update JMeter-command-to-run.sh

--- JMeter-command-to-run.sh
+++ JMeter-command-to-run.sh
@@ -1,1 +1,1 @@
-sudo ./jmeter.sh -n -f -t ./loadtest/Petclinic_loadtest.jmx -l ./loadtest/test_log/petclinic-test-result.jtl -e -o ./loadtest/dashboard
+sudo $(Agent.HomeDirectory)/../../opt/apache-jmeter-5.3/bin/jmeter -n -f -t ./loadtest/Petclinic_loadtest.jmx -l ./loadtest/test_log/petclinic-test-result.jtl -e -o ./loadtest/dashboard
