96cb81ca599c62a15d38edb34b76550826c8adc2
Niels Deen <niels.deen@devoteam.com>
2022-02-28 16:42:42 +0100

    ramped up threads in JMeter test & checking if test can be run without sudo priviliges

--- JMeter-command-to-run.sh
+++ JMeter-command-to-run.sh
@@ -1,1 +1,1 @@
-sudo $(Agent.HomeDirectory)/../../opt/apache-jmeter-5.3/bin/jmeter -n -f -t ./loadtest/petclinic_loadtest.jmx -l ./loadtest/test_log/petclinic-test-result.jtl -e -o ./loadtest/dashboard
+$(Agent.HomeDirectory)/../../opt/apache-jmeter-5.3/bin/jmeter -n -f -t ./loadtest/petclinic_loadtest.jmx -l ./loadtest/test_log/petclinic-test-result.jtl -e -o ./loadtest/dashboard
